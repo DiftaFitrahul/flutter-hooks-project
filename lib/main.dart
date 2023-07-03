46bbeca7628ef31d7312ef299caedcd5e6f6b3a2
Difta Fitrahul Qihaj <difta.fit2002@mail.ugm.ac.id>
2023-07-03 17:08:18 +0700

    finsih step 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
@@ -33,32 +34,71 @@ class MainApp extends StatelessWidget {
 //   }
 // }
 
+// class HomePage extends HookWidget {
+//   const HomePage({super.key});
 
-class HomePage extends HookWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     final controller = useTextEditingController();
+//     final text = useState('');
+//     useEffect(() {
+//       controller.addListener(() {
+//         text.value = controller.text;
+//       });
+//       return null;
+//     }, [controller]);
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("Home Page"),
+//       ),
+//       body: Column(
+//         children: [
+//           TextField(
+//             controller: controller,
+//           ),
+//           Text('you typed : ${text.value}')
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+const uri =
+    'https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2022/06/30/692203986.jpg';
+
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final controller = useTextEditingController();
-    final text = useState('');
-    useEffect(() {
-      controller.addListener(() {
-        text.value = controller.text;
-      });
-      return null;
-    }, [controller]);
+    print('makan');
+    final future = useMemoized(() => NetworkAssetBundle(Uri.parse(uri))
+        .load(uri)
+        .then((value) => value.buffer.asUint8List())
+        .then((value) => Image.memory(value)));
+
+    final snapshot = useFuture(future);
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home Page"),
+        title: Text('makan'),
       ),
       body: Column(
-        children: [
-          TextField(
-            controller: controller,
-          ),
-          Text('you typed : ${text.value}')
-        ],
+        children: [snapshot.data].compactMap().toList(),
       ),
     );
   }
+}
+
+extension CompactMap<T> on Iterable<T?> {
+  Iterable<T> compactMap<E>([
+    E? Function(T?)? transform,
+  ]) =>
+      map(
+        transform ?? (e) => e,
+      ).where((e) => e != null).cast();
+}
+
+void coba() {
+  final data = [1, 2, 3, null, 4];
+  final new_data = data.compactMap();
 }
