2ed962ed4b2bd7eab2bab77906547c7e331e4d8b
Difta Fitrahul Qihaj <difta.fit2002@mail.ugm.ac.id>
2023-07-03 16:35:08 +0700

    finish step 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +10,54 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return const MaterialApp(home: HomePage());
+  }
+}
+
+// Stream<String> getTime() => Stream.periodic(
+//       const Duration(seconds: 1),
+//       (_) {
+//         return DateTime.now().toIso8601String();
+//       },
+//     );
+
+// class HomePage extends StatelessWidget {
+//   const HomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final timeNow = useStream(getTime());
+//     return Scaffold(
+//       appBar: AppBar(title: Text(timeNow.data ?? "Home Page")),
+//     );
+//   }
+// }
+
+
+class HomePage extends HookWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = useTextEditingController();
+    final text = useState('');
+    useEffect(() {
+      controller.addListener(() {
+        text.value = controller.text;
+      });
+      return null;
+    }, [controller]);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: controller,
+          ),
+          Text('you typed : ${text.value}')
+        ],
       ),
     );
   }
